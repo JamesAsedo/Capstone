bf10fd7ec4554751a46cf0bcdc6f5813a824a0c7
James Asedo <james.asedo@gmail.com>
2023-03-28 16:05:00 -0700

    Capstone update

--- components/views/Restaurants.js
+++ components/views/Restaurants.js
@@ -2,6 +2,10 @@ import html from "html-literal";
 import kobePic1 from "/assets/KobeValentine.jpg";
 
 export default state => html`
+  <main>
+    Let's Eat!
+  </main>
+
   <div>
     <form>
       <label for="zip-code">Zip Code:</label>
@@ -9,27 +13,26 @@ export default state => html`
       <button type="submit">Submit</button>
     </form>
   </div>
-  <!-- <img src="${kobePic1}" /> -->
 
-  <main>
-    Woof
-  </main>
-  <table id="results">
-    ${state.yelpArray
-      .map(entry => {
-        return html`
-          <tr>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-          </tr>
-          <td><img class="resultsImg" src="${entry.image_url}" /></td>
-          <td>${entry.name}</td>
-          <td>${entry.location.display_address}</td>
-          <td>${entry.display_phone}</td>
-        `;
-      })
-      .join("")}
-  </table>
+  <div id="results-container">
+    <table id="results">
+      ${state.yelpArray
+        .map(entry => {
+          return html`
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+            <td><img class="resultsImg" src="${entry.image_url}" /></td>
+            <td>${entry.name}</td>
+            <td>${entry.location.display_address}</td>
+            <td>${entry.display_phone}</td>
+          `;
+        })
+        .join("")}
+    </table>
+    <div id="no-restaurant-results" alt="No results"></div>
+  </div>
 `;
--- components/views/Services.js
+++ components/views/Services.js
@@ -1,6 +1,11 @@
 import html from "html-literal";
+import kobePic3 from "/assets/Kobe bee.jpeg";
 
 export default state => html`
+  <main>
+    Woof!
+  </main>
+
   <div>
     <form>
       <label for="zip-code">Zip Code:</label>
@@ -9,25 +14,25 @@ export default state => html`
     </form>
   </div>
 
-  <main>
-    Woof
-  </main>
-  <table id="results">
-    ${state.yelpArray
-      .map(entry => {
-        return html`
-          <tr>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
-          </tr>
-          <td><img class="resultsImg" src="${entry.image_url}" /></td>
-          <td>${entry.name}</td>
-          <td>${entry.location.display_address}</td>
-          <td>${entry.display_phone}</td>
-        `;
-      })
-      .join("")}
-  </table>
+  <div id="results-container">
+    <table id="results">
+      ${state.yelpArray
+        .map(entry => {
+          return html`
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+            <td><img class="resultsImg" src="${entry.image_url}" /></td>
+            <td>${entry.name}</td>
+            <td>${entry.location.display_address}</td>
+            <td>${entry.display_phone}</td>
+          `;
+        })
+        .join("")}
+    </table>
+    <div id="no-service-results" alt="No results"></div>
+  </div>
 `;
